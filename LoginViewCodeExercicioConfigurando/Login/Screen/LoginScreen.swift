c9511da87b3ddea9324eb9a3c23dbc0163becb5b
Higor <92187145+HigorCris21@users.noreply.github.com>
2024-06-26 16:21:49 -0300

    feature/criando botões

--- LoginViewCodeExercicioConfigurando/Login/Screen/LoginScreen.swift
+++ LoginViewCodeExercicioConfigurando/Login/Screen/LoginScreen.swift
@@ -55,6 +55,31 @@ class LoginScreen: UIView {
         return passwordTextField
     }()
     
+    lazy var loginButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Logar", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        button.setTitleColor(.white, for: .normal)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 7.5
+        button.backgroundColor = UIColor(red: 3/255, green: 58/255, blue: 51/255, alpha: 1.0)
+    
+        return button
+        
+    }()
+    
+    lazy var registerButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Não tem conta? Cadastre-se", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        button.setTitleColor(.white, for: .normal)
+     
+        return button
+        
+    }()
+    
     
     //Método construtor
     override init(frame: CGRect) {
@@ -62,6 +87,7 @@ class LoginScreen: UIView {
         self.configBackGround()
         self.configSuperView()
         self.setupConstraints()
+
     }
     
     required init?(coder: NSCoder) {
@@ -79,6 +105,8 @@ class LoginScreen: UIView {
         self.addSubview(self.logoAppImageView)
         self.addSubview(self.emailTextField)
         self.addSubview(self.passwordTextField)
+        self.addSubview(self.loginButton)
+        self.addSubview(self.registerButton)
     }
     
     
@@ -100,7 +128,19 @@ class LoginScreen: UIView {
             self.passwordTextField.topAnchor.constraint(equalTo: self.emailTextField.bottomAnchor, constant: 15),
             self.passwordTextField.leadingAnchor.constraint(equalTo: self.emailTextField.leadingAnchor),
             self.passwordTextField.trailingAnchor.constraint(equalTo: self.emailTextField.trailingAnchor),
-            self.passwordTextField.heightAnchor.constraint(equalTo: self.emailTextField.heightAnchor)
+            self.passwordTextField.heightAnchor.constraint(equalTo: self.emailTextField.heightAnchor),
+            
+            
+            self.loginButton.topAnchor.constraint(equalTo: self.passwordTextField.bottomAnchor, constant: 15),
+            self.loginButton.leadingAnchor.constraint(equalTo: self.emailTextField.leadingAnchor),
+            self.loginButton.trailingAnchor.constraint(equalTo: self.emailTextField.trailingAnchor),
+            self.loginButton.heightAnchor.constraint(equalTo: self.emailTextField.heightAnchor),
+            
+        
+            self.registerButton.topAnchor.constraint(equalTo: self.loginButton.bottomAnchor, constant: 15),
+            self.registerButton.leadingAnchor.constraint(equalTo: self.emailTextField.leadingAnchor),
+            self.registerButton.trailingAnchor.constraint(equalTo: self.emailTextField.trailingAnchor),
+            self.registerButton.heightAnchor.constraint(equalTo: self.emailTextField.heightAnchor)
             
             
         ])
