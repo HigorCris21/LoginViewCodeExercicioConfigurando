afc8dbc7a47b451d320c20be9d6874424f92f95c
Higor <92187145+HigorCris21@users.noreply.github.com>
2024-07-31 16:55:49 -0300

    feature/Criando acao do botao login

--- LoginViewCodeExercicioConfigurando/Login/Screen/LoginScreen.swift
+++ LoginViewCodeExercicioConfigurando/Login/Screen/LoginScreen.swift
@@ -64,6 +64,8 @@ class LoginScreen: UIView {
         button.clipsToBounds = true
         button.layer.cornerRadius = 7.5
         button.backgroundColor = UIColor(red: 3/255, green: 58/255, blue: 51/255, alpha: 1.0)
+        
+        button.addTarget(self, action: #selector(self.tappedLoginButton), for: .touchUpInside)
     
         return button
         
@@ -115,8 +117,9 @@ class LoginScreen: UIView {
         self.passwordTextField.delegate = delegate
     }
     
-    
-    
+    @objc private func tappedLoginButton() {
+        print("Botão de Login!")
+    }
     
     
     private func setupConstraints(){
